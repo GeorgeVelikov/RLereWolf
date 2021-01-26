601ada432c88f66d667b1c18e15aa77473e272cc
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2021-01-26 20:21:57 +0000

    add game to server.py
    Might want to just have Server == Game, but I'll keep it separated for now

--- Werewolf/game/infrastructure/Server.py
+++ Werewolf/game/infrastructure/Server.py
@@ -1,7 +1,8 @@
 from game.infrastructure.ServerBase import ServerBase;
+from game.Game import Game;
 
 class Server(ServerBase):
     def __init(self, name):
         super().__init__();
-
         this.Name = name;
+        this.Game = Game();
