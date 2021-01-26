ebc071162072f64bcf14596429aacf6d1471d4f6
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2021-01-26 20:10:09 +0000

    Add max players constant, add join/leave to game
    this likely belongs to the server, we'll see later down the line

--- Werewolf/game/Game.py
+++ Werewolf/game/Game.py
@@ -5,10 +5,25 @@ import game.Constants as Constant;
 
 class Game():
     def __init__(self):
-        self.Players = list();
         self.Turn = 0;
+        self.Players = list();
         self.TimeOfDay = TimeOfDayEnum._None;
         self.TurnPhase = TurnPhaseTypeEnum._None
+
+    def Join(self, player):
+        self.Players\
+            .append(player)\
+            .sort();
+        return;
+
+    def Leave(self, player):
+        if (player not in self.Players):
+            # TODO: raise some silent exception
+            return;
+
+        # no need to sort, already alphabetical
+        self.Players\
+            .remove(player);
 
     def Start(self):
         if (len(self.Players) < Constant.MINIMAL_PLAYER_COUNT):
