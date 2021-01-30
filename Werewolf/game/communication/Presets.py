ff2cdb6b1c4bf149814ead523cc2cab98e7aee02
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2021-01-30 14:29:21 +0000

    tidy up comm presets

--- Werewolf/game/communication/Presets.py
+++ Werewolf/game/communication/Presets.py
@@ -5,13 +5,15 @@ __ROLE = "%role%";
 YES = "Yes.";
 NO = "No.";
 
-ACCUSE = f"I accuse {__PLAYER} of being a werewolf.";
+ASSERT_PLAYER_ROLE = f"I think {__PLAYER} is a {__ROLE}.";
 DECLARE_ROLE = f"I am a {__ROLE}.";
-KNOW_ROLE = f"I know {__PLAYER} is a {__ROLE}.";
-VOTE = f"I vote to execute {__PLAYER}.";
+VOTE_PLAYER = f"I vote to execute {__PLAYER}.";
 
 # Doctor specific
 DOCTOR_NOT_IN_HOUSE = f"{__PLAYER} was not in their house.";
 
 # Werewolf specific
-WEREWOLF_ATTACK = f"I will attack {__PLAYER}.";
+WEREWOLF_ATTACK_PLAYER = f"I will attack {__PLAYER}.";
+
+# Seer specific
+SEER_DIVINE_PLAYER_ROLE = f"The divines say that {__PLAYER} is a {__ROLE}."
