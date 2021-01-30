f572fd5f55a55d9ff8ab38853f34a403eb02ccf9
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2021-01-30 14:21:36 +0000

    add basic comm presets

---
+++ Werewolf/game/communication/Presets.py
@@ -0,0 +1,17 @@
+__PLAYER = "%player%";
+__ROLE = "%role%";
+
+# General
+YES = "Yes.";
+NO = "No.";
+
+ACCUSE = f"I accuse {__PLAYER} of being a werewolf.";
+DECLARE_ROLE = f"I am a {__ROLE}.";
+KNOW_ROLE = f"I know {__PLAYER} is a {__ROLE}.";
+VOTE = f"I vote to execute {__PLAYER}.";
+
+# Doctor specific
+DOCTOR_NOT_IN_HOUSE = f"{__PLAYER} was not in their house.";
+
+# Werewolf specific
+WEREWOLF_ATTACK = f"I will attack {__PLAYER}.";
