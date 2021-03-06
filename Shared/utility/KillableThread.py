364e860f82e9a2ad436319cd350c8a42d7fad025
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2021-03-06 15:07:00 +0000

    add some exception handing for killable threads

--- Shared/utility/KillableThread.py
+++ Shared/utility/KillableThread.py
@@ -1,4 +1,5 @@
-import threading
+import threading;
+import traceback;
 
 # Heavily inspired from https://stackoverflow.com/a/325528
 class KillableThread(threading.Thread):
@@ -8,16 +9,26 @@ class KillableThread(threading.Thread):
         self.__killEvent = threading.Event();
         self.__sleepIntervalSeconds = sleepIntervalSeconds;
 
+        self.setDaemon(True);
+
     #override
     def run(self):
-        while True:
-            self.__func();
+        try:
+            while True:
+                self.__func();
 
-            # If no kill signal is set, sleep for the interval,
-            # If kill signal comes in while sleeping, immediately
-            # wake up and handle
-            if self.__killEvent.wait(self.__sleepIntervalSeconds):
-                return;
+                # If no kill signal is set, sleep for the interval,
+                # If kill signal comes in while sleeping, immediately
+                # wake up and handle
+                if self.__killEvent.wait(self.__sleepIntervalSeconds):
+                    return;
+        except Exception as error:
+            trace = traceback.format_exc();
+
+            # Log this somewhere for the user?
+            print(str(error) + "\n\n" + trace);
+
+        return;
 
     def Kill(self):
         self.__killEvent.set();
