d541e2f3af0d527f4e96f9f98fd62037c6ced11c
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2021-03-27 20:22:13 +0000

    Add more training rewards

--- Werewolf/environment/TrainingRewards.py
+++ Werewolf/environment/TrainingRewards.py
@@ -1,9 +1,13 @@
 class TrainingRewards():
     DayPassed = +1;
+    Vote = +1;
+    Wait = +0.5;
     Death = -5;
     Victory = +20;
     Lost = -20;
     IncorrectVoteMultiplier = -0.5;
+    DeadPlayerTargeted = -10;
+    IncorrectAction = -10;
 
     def __init__(self):
         pass;
