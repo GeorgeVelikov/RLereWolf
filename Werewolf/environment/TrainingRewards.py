6854d838bb434550efa6dd45179410e1c4524d5f
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2021-05-01 14:36:04 +0100

    bump dead player targeted and incorrect action penalties

--- Werewolf/environment/TrainingRewards.py
+++ Werewolf/environment/TrainingRewards.py
@@ -2,13 +2,13 @@ class TrainingRewards():
     DayPassed = -1;
     Vote = +1;
     Wait = +0.5;
+    IncorrectVoteMultiplier = -0.5;
     Death = -5;
     Victory = +20;
     Lost = -20;
     WerewolfCannibalism = -7.5;
-    IncorrectVoteMultiplier = -0.5;
-    DeadPlayerTargeted = -10;
-    IncorrectAction = -10;
+    DeadPlayerTargeted = -35;
+    IncorrectAction = -35;
 
     def __init__(self):
         pass;
