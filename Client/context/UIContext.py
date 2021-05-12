3b648d58184c4aab08fa7a602d3682096c00dc06
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2021-05-12 17:44:11 +0100

    update some of the functionality images, add more graphs, update manual

--- Client/context/UIContext.py
+++ Client/context/UIContext.py
@@ -55,17 +55,38 @@ class UIContext():
         # TODO: add actual manual, this is more of a
         # placeholder with some minor formatting hints
         manual = \
-            "I. Placeholder Alpha" +\
+            "Welcome to RLereWolf's Client!" +\
             \
-            "\n\t1. Morbi gravida libero nisi, et ultricies sem mattis id. Cras vitae diam ipsum. " +\
-            "Sed porta ac est eget gravida. Praesent mattis, sapien nec laoreet venenatis, metus eros " +\
-            "interdum lacus, id ultrices felis nulla ut nibh. Donec ut dui eu est ornare vestibulum. " +\
-            "Donec suscipit vel nisl in vulputate. Mauris congue iaculis odio, ut tempus nisi porta non. " +\
-            "Morbi eget turpis a nisl suscipit pretium eu quis tortor. Lorem ipsum dolor sit amet, " +\
-            "consectetur adipiscing elit. Mauris commodo feugiat lectus, vitae egestas eros lacinia sed. " +\
-            "Donec interdum mollis magna. Nulla a ex id urna maximus tristique." +\
+            "\n\n" +\
+            "I. What is this?\n" +\
+            "The RLereWolf Client is an implementation of the Werewolf game. This application allows you to play " +\
+            "Werewolf with other human and AI players. In its current iteration only the Villager, Guard, Seer, " +\
+            "and Werewolf roles are implemented. For more information on RLereWolf, visit the URL " +\
+            "github.com/GeorgeVelikov/RLereWolf" +\
+            "\n\n" +\
             \
-            "\nII. Placeholder Beta";
+            "II. What do I do now?\n" +\
+            "In order to start playing with other players, you must set your name/alias, by pressing the " +\
+            "'Set Name' button, located in the main menu. Once you have set your name, you will be able to connect " +\
+            "to the game server!" +\
+            "\n\n" +\
+            \
+            "III. How do I join a game once I am connected?\n" +\
+            "In order to join a specific game, you must select the game you want to join and press the 'Join' " +\
+            "button, located at the bottom right of the screen. Alternatively, you can create your own game by " +\
+            "pressing the 'Create' button in the bottom right of the screen." +\
+            "\n\n" +\
+            \
+            "IV. How do I play the game once I have joined a game?\n" +\
+            "Whenever you join a game, it will be a non-started state as not all of the players are ready! You can " +\
+            "set yourself as ready by pressing the 'Ready' button at the bottom right of the screen. Once everyone " +\
+            "ready, the game will randomly distribute roles and start at day time." +\
+            "\n\n" +\
+            \
+            "V. How do I play my role?\n" +\
+            "The game is based entirely on the base Werewolf game. In order to further familiarize yourself " +\
+            "with the rules and Player roles of Werewolf and RLereWolf, please read the user manual or the " +\
+            "official rule book.";
 
         return self.ShowInformationDialog(manual, "Client Manual");
 
